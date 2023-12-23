0790d7899a02caa45fcc74bd233f3f781aa1d63b
DEMOHCHIK <smnvmaks@gmail.com>
2023-12-23 11:47:16 +0600

    Выполнил 1-ый этап 59 ДЗ

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Status, Type, Task
+from .models import Status, Type, Task, Project
 
 
 @admin.register(Status)
@@ -15,3 +15,8 @@ class TypeAdmin(admin.ModelAdmin):
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['title', 'status', 'add_time', 'edit_time']
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'start_date', 'end_date']
--- webapp/models.py
+++ webapp/models.py
@@ -22,8 +22,19 @@ class Task(models.Model):
                                    validators=[validate_description])
     status = models.ForeignKey('webapp.Status', on_delete=models.PROTECT, verbose_name='Статус')
     types = models.ManyToManyField('webapp.Type', blank=True, related_name='tasks', verbose_name='Типы')
+    project = models.ForeignKey('webapp.Project', on_delete=models.PROTECT, verbose_name='Проект')
     add_time = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     edit_time = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
 
     def __str__(self):
         return self.title
+
+
+class Project(models.Model):
+    start_date = models.DateField(verbose_name='Дата начала')
+    end_date = models.DateField(blank=True, null=True, verbose_name='Дата окончания')
+    name = models.CharField(max_length=70, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
